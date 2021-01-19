a5ee8a2d9eb6688c7d472da7c266077d15b4093e
vinraelll <53097647+vinraelll@users.noreply.github.com>
2021-01-19 17:34:30 +0200

    next section begin

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,8 @@ function browsersync() {
 function styles() {
   return src([
     'app/scss/style.scss',
-    'node_modules/slick-carousel/slick/slick.css'
+    'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
   ])  
   .pipe(scss({outputStyle: 'compressed'}))
   .pipe(concat('style.min.css'))
@@ -36,6 +37,7 @@ function scripts() {
   return src ([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
