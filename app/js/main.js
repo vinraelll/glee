cbfd7ba26b1730318c80dab568b62023558f6107
vinraelll <53097647+vinraelll@users.noreply.github.com>
2021-02-12 17:22:18 +0200

    adaptive homepage

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,26 @@ $(document).ready(function() {
     slidesToScroll: 1,
     arrows: false,
     dots: false,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   })
 
   const mixer = mixitup('.bestseller__items, .new-design__inner');
