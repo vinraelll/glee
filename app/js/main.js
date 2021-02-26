c5d6af35f886d196796e607480906d787b6e424c
vinraelll <53097647+vinraelll@users.noreply.github.com>
2021-02-26 07:04:18 +0200

    shop-list 95%

--- app/js/main.js
+++ app/js/main.js
@@ -23,15 +23,15 @@ $(document).ready(function() {
     }
   });
 
-  const addCart = document.querySelector('.products-list__cart')
-  const addVisible = document.querySelector('.products-list__btn--hidden')
-  const buttons = document.querySelectorAll('.products-list__btn')
-    addCart.addEventListener('click', () => {
-      buttons.forEach((buttons) => {
-        buttons.classList.add('hidden-btn')
-      })
-      addVisible.classList.add('visible-btn')
-    })
+  // const addCart = document.querySelector('.products-list__cart')
+  // const addVisible = document.querySelector('.products-list__btn--hidden')
+  // const buttons = document.querySelectorAll('.products-list__btn')
+  //   addCart.addEventListener('click', () => {
+  //     buttons.forEach((buttons) => {
+  //       buttons.classList.add('hidden-btn')
+  //     })
+  //     addVisible.classList.add('visible-btn')
+  //   })
 
   
   const sidebar = document.querySelector('.sidebar')
